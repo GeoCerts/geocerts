911e7ca9f8d6d50d680ea7263c8779141f57efb6
Nathaniel Bibler <nate@envylabs.com>
2009-09-03 10:30:55 -0400

    Moved testing initializers into test/config/initializers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'geo_certs'
 
 # Load any initializers for testing.
-Dir[File.dirname(__FILE__) + '/initializers/**/*.rb'].sort.each do |initializer|
+Dir[File.dirname(__FILE__) + '/config/initializers/**/*.rb'].sort.each do |initializer|
   require initializer
 end
 
