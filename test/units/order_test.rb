70a652303be39241df450dd9ae1f2e2b05b7e26d
Nathaniel Bibler <nate@envylabs.com>
2009-09-14 11:01:06 -0400

    Added a test for GeoCerts::Warning generation from within an Order

--- test/units/order_test.rb
+++ test/units/order_test.rb
@@ -4,7 +4,16 @@ class OrderTest < Test::Unit::TestCase
   
   context 'Order' do
     
-    
+    should 'generate a collection of GeoCerts::Warnings' do
+      order = GeoCerts::Order.new({:warnings => {
+        :warning => [
+          {:code => '123', :message => 'testing'}
+        ]}
+      })
+      assert !order.warnings.empty?
+      assert_equal 1, order.warnings.size
+      assert order.warnings.all? { |warning| warning.kind_of?(GeoCerts::Warning) }
+    end
     
   end
   
