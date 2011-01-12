e91321a855308d7f9f3968931965aa4dcc14c035
Nathaniel Bibler <nate@envylabs.com>
2011-01-12 14:42:39 -0500

    Switched to VCR for Agreement stubbing

--- test/config/initializers/vcr.rb
+++ test/config/initializers/vcr.rb
@@ -1,4 +1,14 @@
 VCR.config do |c|
   c.stub_with :fakeweb
-  c.cassette_library_dir = File.expand_path("../../../test/fixtures/remote", __FILE__)
+  c.cassette_library_dir = File.expand_path("../../../fixtures/remote", __FILE__)
+  c.default_cassette_options = {:record => :none}
+  c.before_record do |i|
+    i.uri.sub!(GeoCerts.partner_id, '%{PARTNER_ID}')
+    i.uri.sub!(GeoCerts.api_token, '%{API_TOKEN}')
+    i.request.headers.delete("authorization")
+  end
+  c.before_playback do |i|
+    i.uri.sub!('%{PARTNER_ID}', GeoCerts.partner_id)
+    i.uri.sub!('%{API_TOKEN}', GeoCerts.api_token)
+  end
 end
--- test/integrations/agreement_test.rb
+++ test/integrations/agreement_test.rb
@@ -7,26 +7,21 @@ class GeoCerts::AgreementTest < Test::Unit::TestCase
     context 'user agreement' do
       
       should 'return a GeoCerts::Agreement' do
-        managed_server_request :get, '/products.xml', :response => Responses::Product::All do
-          managed_server_request :get, '/products/Q/agreement.xml', :response => Responses::Agreement::Agreement do
-            assert_kind_of(GeoCerts::Agreement, GeoCerts::Product.find('Q').user_agreement)
-          end
+        VCR.use_cassette('product_agreement') do
+          assert_kind_of GeoCerts::Agreement,
+            GeoCerts::Product.find('Q').user_agreement
         end
       end
       
       should 'contain the server-provided agreement text' do
-        managed_server_request :get, '/products.xml', :response => Responses::Product::All do
-          managed_server_request :get, '/products/Q/agreement.xml', :response => Responses::Agreement::Agreement do
-            text = GeoCerts::Product.find('Q').user_agreement.text
-            assert_match(/GeoTrust\(R\) SSL Certificate Subscriber Agreement/, text)
-            assert_match(/YOU MUST READ THIS GEOTRUST SSL CERTIFICATE SUBSCRIBER AGREEMENT/, text)
-            assert_match(/GeoTrust SSL Certificate Subscriber Agreement Version 2.0/, text)
-          end
+        VCR.use_cassette('product_agreement') do
+          text = GeoCerts::Product.find('Q').user_agreement.text
+          assert_match(/SSL Certificate Subscriber Agreement/, text)
         end
       end
       
       should 'return an error for an unrecognized product code' do
-        managed_server_request :get, '/products/BAD/agreement.xml', :response => Responses::Agreement::UnrecognizedProduct do
+        VCR.use_cassette('product_agreement_404') do
           assert_responds_with_exception(GeoCerts::UnprocessableEntity, -90000) do
             GeoCerts::Product.new(:sku => 'BAD').user_agreement
           end
